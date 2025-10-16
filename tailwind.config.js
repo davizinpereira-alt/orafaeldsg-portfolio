36bafd2e0c2ee30573d48ff33222adb66221641a
davizinpereira-alt <davizinpereira@gmail.com>
2025-10-15 23:51:48 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4C9FF0',
+      }
+    },
+  },
+  plugins: [],
+}
